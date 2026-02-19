f18c9507acfa3229565d34ec078a832ac140578e
EMANUEL Popescu <emanuelstefan192@gmail.com>
2026-02-19 16:57:55 +0200

    forgot to declare function in header

--- src/main.c
+++ src/main.c
@@ -8,19 +8,13 @@ int main(void) {
 
     
     GPIO_Init(LED_BUILTIN, GPIO_OUTPUT);
-<<<<<<< HEAD
-
     uint32_t last_time = 0;
 
-=======
-    int state = 0;
->>>>>>> d704f37 (changed baudrate,com port and solved main toggle problems)
     while (1) {
             
         if (Millis() - last_time >= 1000) {
             last_time = Millis();
             GPIO_Toggle(LED_BUILTIN);
-            DelayT0(1);
         }
     }
 }
